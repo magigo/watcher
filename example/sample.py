dbe70791dd0f976a45f5482b42fc4298efec1b02
纪路 <magi-go@126.com>
2017-05-22 15:26:07 +0800

    add example

--- example/sample.py
+++ example/sample.py
@@ -5,13 +5,14 @@ from __future__ import print_function
 import os
 import logging
 import logging.handlers
+import time
 
 from watcher import CloudWatchLogHandler
 
-LOG = logging.getLogger('Spider')
+LOG = logging.getLogger('Test')
 LOG.setLevel(logging.DEBUG)
 fh = logging.handlers.RotatingFileHandler(
-    '{}/kw_ads_{}.log'.format('/mnt', os.getpid()), maxBytes=100000000, backupCount=5, encoding='utf8')
+    '{}/test_{}.log'.format('/tmp', os.getpid()), maxBytes=100000000, backupCount=5, encoding='utf8')
 fh.setFormatter(
     logging.Formatter(
         '[%(asctime)s] {} [%(processName)s] %(filename)s [line:%(lineno)d] %(levelname)s %(message)s'.format(
@@ -20,5 +21,10 @@ LOG.addHandler(fh)
 LOG.addHandler(CloudWatchLogHandler())
 
 LOG.info('start')
+
+for x in range(100):
+    LOG.info('log num: {}'.format(x))
+    time.sleep(1)
+
 if __name__ == '__main__':
     pass
